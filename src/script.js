d172706e717ced764a6052f79a1d84187f47723d
fbibi4 <127233513+fbibi4@users.noreply.github.com>
2023-03-09 18:48:15 +0000

    Worked on the weather icon

--- src/script.js
+++ src/script.js
@@ -83,3 +83,10 @@ let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
 searchCity("London");
+
+let iconElement = document.querySelector("#icon");
+iconElement.setAttribute(
+  "src",
+  `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+);
+iconElement.setAttribute("alt", response.data.weather[0].description);
